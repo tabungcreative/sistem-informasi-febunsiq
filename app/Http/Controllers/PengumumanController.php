127440750dbb4e3b595e263da2de73503da0b0cc
Golummn <riza.sh15@gmail.com>
2022-09-03 18:48:43 +0700

    add pengumuman

---
+++ app/Http/Controllers/PengumumanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengumuman;
+use Illuminate\Http\Request;
+
+class PengumumanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pengumuman  $pengumuman
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengumuman $pengumuman)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pengumuman  $pengumuman
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pengumuman $pengumuman)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pengumuman  $pengumuman
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengumuman $pengumuman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pengumuman  $pengumuman
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengumuman $pengumuman)
+    {
+        //
+    }
+}
